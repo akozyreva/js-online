98717394dbc79718329b34fa46682d1896fb35cd
akozyreva <anna.ko4523546@gmail.com>
2019-07-24 10:36:10 +0200

    create proto for compiler

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!--<img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+    <div>
+      <p>CSS</p>
+      <textarea v-model="cssCode" rows="10" cols="50"></textarea>
+    </div>
+    <div>
+       <p>Js</p>
+      <textarea v-model="jsCode" rows="10" cols="50"></textarea>
+    </div>
+    <div>
+      <p>html</p>
+      <textarea v-model="htmlCode" rows="10" cols="50"></textarea>
+    </div>
+    
+    <button @click="addContent()">Run</button>
+    <iframe id="finalIframe">
+    </iframe>
   </div>
 </template>
 
@@ -12,6 +28,23 @@ export default {
   name: "app",
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      htmlCode: '',
+      cssCode: '',
+      jsCode: 'aa'
+    }
+  },
+  methods: {
+    addContent() {
+      let x = document.getElementById("finalIframe").contentWindow.document;
+      x.open();
+      console.log(this.htmlCode, this.jsCode)
+      x.write(this.htmlCode);
+      x.write(`<script>${this.jsCode}<\/script>`)
+      x.close();
+    }
   }
 };
 </script>
